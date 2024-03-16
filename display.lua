763ec6821fde7fa362e8d79e8a210d484f5ae920
Sarahtoma12354 <sarahtoma12354@gmail.com>
2024-03-16 10:12:55 -0700

    Update display.lua

--- display.lua
+++ display.lua
@@ -3,13 +3,13 @@ local monitor = peripheral.find("monitor")
 monitor.setCursorPos(1, 1)
 monitor.clear()
 
-while x < 4 then
+while x < 4 do
     monitor.write("Hello world")
     x = x + 1
     os.sleep(0.5)
 end
 
-while y < 4 then
+while y < 4 do
     monitor.scroll(1)
     y = y + 1
     os.sleep(0.5)
