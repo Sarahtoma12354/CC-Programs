4a481ddd9f77e21d8f5240ac447373ca28dd1c00
Sarahtoma12354 <sarahtoma12354@gmail.com>
2024-03-16 10:10:53 -0700

    Update display.lua

--- display.lua
+++ display.lua
@@ -5,9 +5,11 @@ monitor.setCursorPos(1, 1)
 while x < 4 then
     monitor.write("Hello world")
     x = x + 1
+    os.sleep(0.5)
 end
 
 while y < 4 then
     monitor.scroll(1)
     y = y + 1
+    os.sleep(0.5)
 end
